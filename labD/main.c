8cc20c68cb610e5ca5a431a7ebe809653e3bcb57
lublumishunechku <tachilina.ma@edu.spbstu.ru>
2025-11-14 23:10:48 +0300

    Fix main.c syntax and complete labD solution

--- labD/main.c
+++ labD/main.c
@@ -1,21 +1,21 @@
-#include stdio.h
-#include stdlib.h
-#include ruckzack.h
-#include io.h
+#include <stdio.h>
+#include <stdlib.h>
+#include "ruckzack.h"
+#include "io.h"
 
 int main() {
     int n, B, K;
-    int w, v;
-    
-    if (!read_input(input.txt, &n, &B, &K, &w, &v)) {
+    int *w, *v;
+
+    if (!read_input("input.txt", &n, &B, &K, &w, &v)) {
         return 1;
     }
-    
-    int result = (int)malloc(n  sizeof(int));
+
+    int* result = (int*)malloc(n * sizeof(int));
     int count = knapsack_solve(n, B, K, w, v, result);
-    
-    write_output(output.txt, result, count);
-    
+
+    write_output("output.txt", result, count);
+
     free(w);
     free(v);
     free(result);
