55bd54f1f98200c5a6989d3dfc81d78571b0a002
Aleksandr Kasimov <sasha.asimo@ya.ru>
2025-11-20 15:17:20 +0300

    minor update

--- LAB_D/VertexCover.h
+++ LAB_D/VertexCover.h
@@ -5,39 +5,33 @@
 #ifndef VERTEX_COVER_H
 #define VERTEX_COVER_H
 
-
-// Структура для представления ребра графа
 typedef struct {
-    int u;  // первая вершина ребра
-    int v;  // вторая вершина ребра
+    int u;
+    int v;
 } Edge;
 
-// Структура для представления графа
+
 typedef struct {
-    Edge* edges;       // массив ребер
-    int edge_count;    // количество ребер
-    int vertex_count;  // количество вершин
+    Edge* edges;
+    int edge_count;
+    int vertex_count;
 } Graph;
 
-// Структура для представления доминирующего множества
+
 typedef struct {
-    int* vertices;    // массив вершин множества
-    int count;        // количество вершин в множестве
+    int* vertices;
+    int count;
 } VertexCover;
 
-// Функции для работы с графом
+
 Graph* init_graph();
 void free_graph(Graph* g);
 Graph* load_graph(char* filename, int* k);
-
-// Функции для работы с доминирующим множеством
 VertexCover* init_vertex_cover(void);
 void free_vertex_cover(VertexCover* ds);
 void add_vertex(VertexCover* ds, int vertex);
 int check_cover(Graph* g, VertexCover* ds);
 VertexCover* find_vertex_cover(Graph* g, int k, int current_vertex);
-
-// Вспомогательные функции
 void write_vertex_cover(VertexCover* ds, char* filename);
 
 
