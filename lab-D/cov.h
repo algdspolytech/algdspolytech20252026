fa41606470decb1a06827e221d4bae6c6cdae2b0
KseniyaS-D <shitikova.kd@edu.spbstu.ru>
2025-12-17 00:53:33 +0300

    Лабораторная работа D

---
+++ lab-D/cov.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#ifndef COV_H
+#define COV_H
+
+#include <stdbool.h>
+
+#define MAX_VERTICES 50  // Maximum number of vertices
+
+typedef struct {
+    int u, v;
+} Edge;
+
+typedef struct {
+    int vertices_count;
+    int edges_count;
+    Edge edges[MAX_VERTICES * MAX_VERTICES];
+} Graph;
+
+bool read_graph(const char* filename, Graph* graph, int* K);
+bool write_result(const char* filename, int* cover_vertices, int cover_size);
+bool find_vertex_cover(Graph* graph, int K, int* cover_vertices, int* cover_size);
+
+#endif
